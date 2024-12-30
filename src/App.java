96570a91dff3d0b9bbb90eecd260dc2666c30893
aarya <aarya.singh@ucdconnect.ie>
2024-12-30 17:45:43 +0000

    created JPanel and constructor for the game(containing images)

--- src/App.java
+++ src/App.java
@@ -11,11 +11,18 @@ public class App {
         int boardHeight= rowCount*tileSize;
 
         JFrame frame= new JFrame("Pac Man");
-        frame.setVisible(true);//make the window visible
+        //frame.setVisible(true);//make the window visible
         frame.setSize(boardWidth, boardHeight);//set the size of the window
         frame.setLocationRelativeTo(null);//center the window
         frame.setResizable(false);//user can't resize the window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close the window when user click the close button(or the X button)
 
+        PacMan pacmanGame= new PacMan();// represents the JPanel
+        frame.add(pacmanGame);//add the JPanel to the JFrame
+        frame.pack();
+        frame.setVisible(true);
+
     }
+
+    
 }
