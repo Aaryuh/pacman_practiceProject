03d89a84d81b7d8999ce98d9e1be95bd5f13d447
aarya <aarya.singh@ucdconnect.ie>
2024-12-30 17:06:54 +0000

    initial commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int rowCount= 21;
+        int columnCount= 19;
+        int tileSize= 32;
+        int boardWidth= columnCount*tileSize;
+        int boardHeight= rowCount*tileSize;
+
+        JFrame frame= new JFrame("Pac Man");
+        frame.setVisible(true);//make the window visible
+        frame.setSize(boardWidth, boardHeight);//set the size of the window
+        frame.setLocationRelativeTo(null);//center the window
+        frame.setResizable(false);//user can't resize the window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close the window when user click the close button(or the X button)
+
+    }
+}
